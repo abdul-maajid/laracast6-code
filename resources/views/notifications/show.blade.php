ab5619c1477515de3446d861fa63d7bce721ecd2
Maajid Siddiqui <msiddiqui.digitals@gmail.com>
2020-01-17 15:59:43 +0500

    Done diff changes

--- resources/views/notifications/show.blade.php
+++ resources/views/notifications/show.blade.php
@@ -5,9 +5,11 @@
 <div class="container">
     <ul>
         @forelse($notifications as $notif)
+        
             @if($notif->type === 'App\Notifications\PaymentReceived')
                 <li>We have received a payment of ${{ $notif->data['amount'] }} from you.</li>
             @endif
+
             @empty
                 <li>You dont have any notification at the moment</li>
 
